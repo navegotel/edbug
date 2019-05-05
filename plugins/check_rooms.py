bcff617594e6ce990da8073d0b0e3151445b4cba
Markus Barth <markus@markusbarth.net>
2019-05-05 07:15:45 +0200

    added check for GlobalTypes

--- plugins/check_rooms.py
+++ plugins/check_rooms.py
@@ -13,6 +13,8 @@ def room_checkroomcode(roomnode):
 def room_checkdescriptions(roomnode):
     errormsgs = list()
     descriptionnodes = roomnode.findall("edf:Descriptions/edf:Description", ns)
+    if len(descriptionnodes) == 0:
+        raise RoomError("Consider including room descriptions in the Room node", level=logging.INFO)
     hasdefaultlang = False
     for descriptionnode in descriptionnodes:
         lang = descriptionnode.get("Language")
@@ -26,5 +28,30 @@ def room_checkdescriptions(roomnode):
         errormsgs.append(ErrorMsg("At least on Language attribute in Descriptions should be set to ** to be marked as fallback language", level=logging.WARNING, node=descriptionnode))
     if len(errormsgs) > 0:
         raise RoomError("{0} errors in Descriptions".format(len(errormsgs)), messages=errormsgs)
-
-# TODO Board, GobalTypes, find a way to check Restrictions on SellingData and Room level
+        
+def room_checkboard(roomnode):
+    errormsgs = list()
+    boardnodes = roomnode.findall("edf:Boards/edf:Board", ns)
+    if len(boardnodes) == 0:
+        raise RoomError("You must define at least one Boards/Board element", level=logging.ERROR)
+    for boardnode in boardnodes:
+        if boardnode.get("Code") is None:
+            errormsgs.append("No Code attribute in Board node", node=boardnode, level=logging.INFO)
+        if boardnode.get("GlobalType") is None:
+            errormsgs.append("GlobalType attribute in Board node is mandatory", node=boardnode, level=logging.ERROR)
+        elif boardnode.get("GlobalType") not in ['AO', 'BB', 'HB', 'HB+', 'FB', 'FB+', 'SC', 'AI', 'AI+', 'XX']:
+            errormsgs.append(ErrorMsg("GlobalType attribute value must be one of 'AO', 'BB', 'HB', 'HB+', 'FB', 'FB+', 'SC', 'AI', 'AI+', 'XX'", node=boardnode, level=logging.ERROR))
+        if len(errormsgs) > 0:
+            raise RoomError("{0} errors in Boards node".format(len(errormsgs)), messages=errormsgs)
+        
+def room_checkglobaltypes(roomnode):
+    errormsgs = list()
+    gtnodes = roomnode.findall("edf:GlobalTypes/edf:GlobalType", ns)
+    if len(gtnodes) == 0:
+        raise RoomError("GlobalTypes element should contain at least one GlobalType element", level=logging.WARNING)
+    for gtnode in gtnodes:
+        code = gtnode.get("Code")
+        if code is None:
+            errormsgs.append("Empty Code attribute in GlobalType element", node=gtnode, level=logging.ERROR)
+        elif code not in ['AP', 'BU', 'CA', 'CH', 'CT', 'DP', 'DR', 'DL', 'ER', 'FC', 'FR', 'HA', 'HB', 'JS', 'MA', 'MB', 'MH', 'PH', 'SP', 'SR', 'ST', 'SU', 'TR', 'VF', 'VH', 'VI', 'WB', 'XX']:
+            errormsgs.append(ErrorMsg("Code attribute value in GlobalType element must be one of 'AP', 'BU', 'CA', 'CH', 'CT', 'DP', 'DR', 'DL', 'ER', 'FC', 'FR', 'HA', 'HB', 'JS', 'MA', 'MB', 'MH', 'PH', 'SP', 'SR', 'ST', 'SU', 'TR', 'VF', 'VH', 'VI', 'WB', 'XX'", node=gtnode, level=logging.ERROR))
